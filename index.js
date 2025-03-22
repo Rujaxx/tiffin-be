203b5c2b7137c43367358f8555f133053f886ff5
Rujaxx <shaikhrujan@gmail.com>
2025-03-22 17:24:06 +0530

    error handling and health check added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config()
 const cors = require('cors')
 const connectDb = require('./db/connectDb.js')
 connectDb()
+
 app.use(cors())
 app.use(express.urlencoded({extended:true}))    
 
@@ -13,12 +14,21 @@ app.use(express.json())
 const authRoutes = require("./routes/auth.route.js")
 const adminRoutes = require("./routes/admin.route.js")
 const bookTiffinRoutes = require("./routes/bookTiffin.route.js")
+// hea;th check
+app.get("/",(req,res)=>{
+    res.json({message:"Server is running"})
+})
 app.use(authRoutes)
 app.use(adminRoutes)
 app.use("/tiffins",bookTiffinRoutes)
 
 app.use((req,res,next)=>{
     res.status(404).json({message:"Route doesn't exist"})
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err)
+    res.status(500).json({message:"Something went wrong"})
 })
 
 app.listen(PORT,() => {
